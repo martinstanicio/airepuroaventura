e8233c11a85998035cad1624bfe0e2ab7bdfb33d
Martín Stanicio <martuytimo@gmail.com>
2023-10-08 19:22:37 -0300

    create navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
+import Link from "next/link";
+
+import ThemeToggle from "./theme-toggle";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+
+const links = [
+  { label: "Inicio", path: "/" },
+  { label: "Salidas", path: "/salidas" },
+  { label: "Educación", path: "/educacion" },
+  { label: "Contacto", path: "/contacto" },
+];
+
+export default function Navbar() {
+  return (
+    <nav>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon" className="md:hidden">
+            <HamburgerMenuIcon className="aspect-square h-4" />
+            <span className="sr-only">Menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          {links.map(({ label, path }, i) => (
+            <DropdownMenuItem key={i}>
+              <Link href={path}>{label}</Link>
+            </DropdownMenuItem>
+          ))}
+          <DropdownMenuSeparator />
+          <ThemeToggle />
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <NavigationMenu className="hidden md:block">
+        <NavigationMenuList>
+          {links.map(({ label, path }, i) => (
+            <NavigationMenuItem key={i}>
+              <Link href={path} legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  {label}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+          <ThemeToggle />
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
+  );
+}
