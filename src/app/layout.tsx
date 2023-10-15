7af9bab9875c4dba3a326c0a74d294062456b84c
Martín Stanicio <martuytimo@gmail.com>
2023-10-15 19:29:24 -0300

    install font awesome brands

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
+import { config as faConfig } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import "@/styles/globals.css";
+
+faConfig.autoAddCss = false;
 
 const font = Inter({ subsets: ["latin"] });
 
