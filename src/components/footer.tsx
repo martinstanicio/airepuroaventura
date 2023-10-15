7c196737b6fda2b8b0358beaaa82d0bcd20a7520
Martín Stanicio <martuytimo@gmail.com>
2023-10-15 20:03:09 -0300

    add social links to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,45 @@
+import {
+  faFacebookSquare,
+  faInstagram,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import Brand from "./brand";
+
+const social = [
+  {
+    label: "Facebook",
+    href: "https://www.facebook.com/profile.php?id=100054648900632",
+    icon: faFacebookSquare,
+  },
+  {
+    label: "Instagram",
+    href: "https://www.instagram.com/airepuroaventura/",
+    icon: faInstagram,
+  },
+  {
+    label: "WhatsApp",
+    href: "https://wa.me/message/WMZXPI7AYTWNL1/",
+    icon: faWhatsapp,
+  },
+];
 
 export default function Footer() {
   return (
     <footer className="dark bg-black text-foreground">
       <div className="mx-auto max-w-4xl space-y-12 px-4 py-8">
-        <Brand />
+        <div className="flex flex-col justify-between gap-x-4 gap-y-12 sm:flex-row sm:items-center">
+          <Brand />
+          <div className="flex justify-evenly gap-6">
+            {social.map(({ label, href, icon }, i) => (
+              <a href={href} target="_blank" key={i}>
+                <FontAwesomeIcon icon={icon} className="h-6" />
+                <span className="sr-only">{label}</span>
+              </a>
+            ))}
+          </div>
+        </div>
         <div className="flex flex-wrap justify-between gap-4">
           <p>Aire Puro Aventura &copy; {new Date().getFullYear()}</p>
           <p>
