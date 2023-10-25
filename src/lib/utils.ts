40b645dfd1e2b04adf6eda1db98d8041ff237cd2
Martín Stanicio <martuytimo@gmail.com>
2023-10-25 01:05:47 -0300

    add currency formating util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const ARS = new Intl.NumberFormat("es-AR", {
+  style: "currency",
+  currency: "ARS",
+  maximumFractionDigits: 0,
+});
