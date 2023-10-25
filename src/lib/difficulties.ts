6b52172d1ef99b61d5b7bf0ccd55bed97dc3b73f
Martín Stanicio <martuytimo@gmail.com>
2023-10-25 20:59:12 -0300

    change medium difficulty display name

--- src/lib/difficulties.ts
+++ src/lib/difficulties.ts
@@ -1,5 +1,5 @@
 export default [
   { value: "easy", name: "facil", className: "bg-easy" },
-  { value: "medium", name: "moderada", className: "bg-medium" },
+  { value: "medium", name: "moderado", className: "bg-medium" },
   { value: "hard", name: "dificil", className: "bg-hard" },
 ] as const;
