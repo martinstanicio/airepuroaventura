4403b9cc5be5dbc3d93d05483e9fd1aa48891b22
Martín Stanicio <martuytimo@gmail.com>
2023-10-25 21:32:31 -0300

    add color to social icons

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -14,16 +14,19 @@ const social = [
     label: "Facebook",
     href: "https://www.facebook.com/profile.php?id=100054648900632",
     icon: faFacebookSquare,
+    color: "#0866FF",
   },
   {
     label: "Instagram",
     href: "https://www.instagram.com/airepuroaventura/",
     icon: faInstagram,
+    color: "#E4405F",
   },
   {
     label: "WhatsApp",
     href: "https://wa.me/message/WMZXPI7AYTWNL1/",
     icon: faWhatsapp,
+    color: "#25D366",
   },
 ] as const;
 
@@ -32,10 +35,14 @@ export type Props = React.HTMLAttributes<HTMLDivElement>;
 export default function SocialIcons({ className, ...props }: Props) {
   return (
     <div className={cn("flex justify-evenly gap-6", className)} {...props}>
-      {social.map(({ label, href, icon }, i) => (
+      {social.map(({ label, href, icon, color }, i) => (
         <Button asChild variant="ghost" size="icon" key={i}>
           <a href={href} target="_blank">
-            <FontAwesomeIcon icon={icon} className="text-2xl" />
+            <FontAwesomeIcon
+              icon={icon}
+              className="text-2xl text-[var(--icon-color)] dark:text-foreground"
+              style={{ "--icon-color": color }}
+            />
             <span className="sr-only">{label}</span>
           </a>
         </Button>
