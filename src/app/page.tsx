392bf985b94d39b0bd20182003427e656aa3be9a
Martín Stanicio <martuytimo@gmail.com>
2023-11-01 23:32:15 -0300

    create about us section in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,35 @@ export default function Home() {
     <>
       <Hero />
       <main>
+        <div className="bg-secondary">
+          <section className="container grid max-w-4xl gap-8 py-8 md:grid-cols-2">
+            <div className="relative h-full max-md:aspect-[4/3]">
+              <Image
+                src="/about-us.jpg"
+                alt="Todos los participantes de una salida de cicloturismo al llegar a destino"
+                fill
+                className="overflow-hidden rounded-md object-cover object-center shadow"
+              />
+            </div>
+            <div className="prose prose-a:no-underline">
+              <h2>Quiénes somos</h2>
+              <p>
+                Nosotros somos{" "}
+                <span className="font-bold text-primary">Aire Puro</span>, y
+                nuestra pasión es conectar a las personas con la belleza natural
+                y la aventura que ofrece <strong>Argentina</strong>.
+              </p>
+              <p>
+                Con nuestros <strong>guías experimentados</strong>, estamos
+                comprometidos a brindar a nuestros viajeros{" "}
+                <strong>aventuras inolvidables</strong>.
+              </p>
+              <Button asChild className="max-md:w-full">
+                <Link href="/sobre-nosotros">Sobre nosotros</Link>
+              </Button>
+            </div>
+          </section>
+        </div>
         <section className="container space-y-8 py-8">
           <header className="prose md:text-center">
             <h2>Próximas salidas</h2>
@@ -24,11 +53,11 @@ export default function Home() {
               {upcomingSalidas.map((salida, i) => (
                 <SalidaCard className="mb-4" key={i} {...salida} />
               ))}
-          </div>
-          <div className="flex justify-center">
-          <Button asChild size="lg" className="max-md:w-full">
-            <Link href="/salidas">Ver todas las salidas</Link>
-          </Button>
+            </div>
+            <div className="flex justify-center">
+              <Button asChild size="lg" className="max-md:w-full">
+                <Link href="/salidas">Ver todas las salidas</Link>
+              </Button>
             </div>
           </div>
         </section>
