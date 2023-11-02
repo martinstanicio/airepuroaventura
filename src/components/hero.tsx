addb47e094465e2648bd4ff1f571e099ed68e2bc
Martín Stanicio <martuytimo@gmail.com>
2023-11-02 20:12:24 -0300

    set hero max width

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,14 +7,14 @@ import { Button } from "./ui/button";
 
 export default function Hero() {
   return (
-    <section className="relative grid md:grid-cols-2">
-      <div className="relative grid place-items-center md:order-last md:pb-16 md:pl-8 md:pr-4 md:pt-4">
+    <section className="relative mx-auto grid max-w-6xl md:grid-cols-2">
+      <div className="relative grid place-items-center md:order-last md:pb-16 md:pl-8 md:pr-4 md:pt-16">
         <svg
           viewBox="0 0 100 63"
           className="absolute bottom-0 right-0 h-full max-md:hidden"
         >
           <path
-            d="M100 3C100 3 82.5 3 80 13.5C75.9676 26.4784 86.5 41.5 70.5 54C55.7753 65.5037 41.5 38 20.5 49C11.9123 53.4983 10 63 10 63"
+            d="M99 3C99 3 82.5 3 80 13.5C75.9676 26.4784 86.5 41.5 70.5 54C55.7753 65.5037 41.5 38 20.5 49C11.9123 53.4983 10 63 10 63"
             className="fill-none stroke-logo-yellow stroke-2"
             strokeDasharray="5"
             strokeLinecap="round"
@@ -31,7 +31,7 @@ export default function Hero() {
           />
         </div>
       </div>
-      <div className="prose relative z-10 flex max-w-md flex-col justify-center px-4 pb-16 pt-4 prose-a:no-underline md:pr-0">
+      <div className="prose relative z-10 flex max-w-md flex-col justify-center px-4 pb-16 pt-8 prose-a:no-underline md:pr-0 md:pt-16">
         <h1 className="[text-wrap:balance]">{title}</h1>
         <p className="lead mt-0 md:w-3/4">
           Participá de inolvidables excursiones y conocé más de la cultura de
