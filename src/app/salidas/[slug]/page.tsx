34e1ed6888745a529992b978c688dcd3ae9a234d
Martín Stanicio <martuytimo@gmail.com>
2023-11-09 23:51:55 -0300

    add sidebar to salida page

--- src/app/salidas/[slug]/page.tsx
+++ src/app/salidas/[slug]/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
+import { CheckoutForm } from "@/components/checkout-form";
 import DifficultyBadge from "@/components/difficulty-badge";
 import { Badge } from "@/components/ui/badge";
 import { allSalidasSlugs, findSalida } from "@/lib/salidas";
-import { longDate } from "@/lib/utils";
+import { ARS, longDate } from "@/lib/utils";
 
 export const generateStaticParams = () => allSalidasSlugs;
 
@@ -27,28 +28,48 @@ export default function SalidaPage({ params }: Props) {
   );
 
   return (
-    <article className="container prose py-8">
-      <header>
-        <p className="lead">{longDate.format(new Date(date))}</p>
-        <h1>{title}</h1>
-        <div className="relative flex flex-wrap gap-2">
-          <DifficultyBadge difficulty={difficulty} />
-          {tags.map((tag, i) => (
-            <Badge key={i} variant="secondary">
-              {tag}
-            </Badge>
-          ))}
-        </div>
-        <div className="aspect-[4/3] w-full">
-          <Image
-            src={img}
-            alt={title}
-            fill
-            className="!static rounded-md object-cover object-center"
-          />
-        </div>
-      </header>
-      <main dangerouslySetInnerHTML={{ __html: salida.body.html }} />
+    <article className="container relative mx-auto grid max-w-6xl grid-cols-1 gap-8 py-8 lg:grid-cols-3">
+      <div className="prose lg:col-span-2">
+        <header>
+          <p className="lead mt-0">{longDate.format(new Date(date))}</p>
+          <h1>{title}</h1>
+          <div className="relative flex flex-wrap gap-2">
+            <DifficultyBadge difficulty={difficulty} />
+            {tags.map((tag, i) => (
+              <Badge key={i} variant="secondary">
+                {tag}
+              </Badge>
+            ))}
+          </div>
+          <div className="aspect-[4/3] w-full">
+            <Image
+              src={img}
+              alt={title}
+              fill
+              className="!static rounded-md object-cover object-center"
+            />
+          </div>
+        </header>
+        <main dangerouslySetInnerHTML={{ __html: salida.body.html }} />
+      </div>
+
+      <div className="space-y-8 place-self-start lg:sticky lg:top-24">
+        <section className="overflow-hidden rounded-sm text-center shadow">
+          <p className="bg-primary p-4 text-xl font-bold text-primary-foreground">
+            {ARS.format(price)}
+          </p>
+          <p className="bg-secondary p-2">Precio por persona</p>
+        </section>
+
+        <section className="prose rounded-sm bg-secondary p-4 shadow">
+          <h2 className="mt-0">Realizá tu reserva</h2>
+          <p>
+            ¡Reserva tu lugar ahora para no perderte de esta extraordinaria
+            aventura!
+          </p>
+          <CheckoutForm className="not-prose" />
+        </section>
+      </div>
     </article>
   );
 }
---
+++ src/components/checkout-form.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { cn } from "@/lib/utils";
+
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+
+export type Props = React.HTMLAttributes<HTMLFormElement>;
+
+const formSchema = z.object({
+  firstName: z
+    .string({
+      required_error: "Por favor ingrese su nombre.",
+    })
+    .min(1, "Por favor ingrese su nombre.")
+    .trim(),
+  lastName: z
+    .string({
+      required_error: "Por favor ingrese su apellido.",
+    })
+    .min(1, "Por favor ingrese su apellido.")
+    .trim(),
+  email: z
+    .string({ required_error: "Por favor ingrese un email." })
+    .email("Por favor ingrese un email válido.")
+    .trim(),
+  tel: z
+    .string({ required_error: "Por favor ingrese su número de teléfono." })
+    .min(1, "Por favor ingrese su número de teléfono.")
+    .trim(),
+});
+
+export function CheckoutForm({ className, ...props }: Props) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn("space-y-8", className)}
+        {...props}
+      >
+        <FormField
+          control={form.control}
+          name="firstName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nombre</FormLabel>
+              <FormControl>
+                <Input placeholder="Carlos" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Apellido</FormLabel>
+              <FormControl>
+                <Input placeholder="García" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="carlos@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="tel"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Teléfono</FormLabel>
+              <FormControl>
+                <Input type="tel" placeholder="+54 9 348 401-2345" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="w-full">
+          Reservar
+        </Button>
+      </form>
+    </Form>
+  );
+}
