aff09559af418647220b3f2d7b0f3ccf104982d0
Martín Stanicio <martuytimo@gmail.com>
2024-05-26 16:57:17 -0300

    add contact form to contact page

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,9 +1,9 @@
 import { Metadata } from "next";
 
-// import { ContactForm } from "@/components/contact-form";
+import ContactForm from "@/components/contact-form";
 import SocialIcons from "@/components/social-icons";
-
-// import { Separator } from "@/components/ui/separator";
+import { Separator } from "@/components/ui/separator";
+import { formatPhoneNumber, getWhatsAppLink } from "@/lib/whatsapp";
 
 const title = "Contactanos";
 const description =
@@ -19,21 +19,35 @@ export const metadata: Metadata = {
 
 export default function Contacto() {
   return (
-    <main className="container space-y-8 py-8">
+    <main className="container max-w-prose space-y-8 py-8">
       <header className="prose">
         <h1>{title}</h1>
-        <p>{description}</p>
+        <p>
+          Puedes comunicarte con nosotros mediante WhatsApp (
+          <a href={getWhatsAppLink(+process.env.NEXT_PUBLIC_PHONE).toString()}>
+            {formatPhoneNumber(+process.env.NEXT_PUBLIC_PHONE, 2, 1, 2)}
+          </a>
+          ), o el formulario que encontrarás a continuación. También puedes
+          encontrarnos en Instagram y Facebook.
+        </p>
         <SocialIcons />
         <p>
           Nos comprometemos a responder tu mensaje lo antes posible, para
-          brindarte la información y el apoyo que necesitas.
+          brindarte la información y el apoyo que necesites.
         </p>
       </header>
-      {/* <Separator />
+
+      <Separator />
+
       <section className="prose">
         <h2>Formulario</h2>
+        <p>
+          Al enviar el formulario serás redirijido a WhatsApp. En caso de tener
+          inconvenientes, puedes comunicarte mediante los medios detallados
+          arriba.
+        </p>
         <ContactForm />
-      </section> */}
+      </section>
     </main>
   );
 }
