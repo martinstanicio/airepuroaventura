bb2391f042fa85f40fe4ba462fed6c0198acf8ee
Martín Stanicio <martuytimo@gmail.com>
2024-05-26 20:22:37 -0300

    remove extra else statement

--- src/app/salidas/[slug]/inscripcion/page.tsx
+++ src/app/salidas/[slug]/inscripcion/page.tsx
@@ -29,35 +29,35 @@ export default function InscripcionPage({ params }: Props) {
   const { title, date, price } = salida;
 
   if (isPastSalida(salida)) return notFound();
-  else
-    return (
-      <main className="container mx-auto space-y-8 py-8">
-        <section className="prose">
-          <p className="lead">{title}</p>
-          <h1>Inscripción</h1>
-          <p>
-            Formulario de inscripción para realizar la actividad{" "}
-            <Link href={`/${salida.url}`}>{title}</Link>, con fecha de inicio el{" "}
-            <strong className="text-primary">
-              {longDate.format(new Date(date))}
-            </strong>
-            .
-          </p>
-        </section>
-        <section className="mx-auto max-w-prose overflow-hidden rounded-sm text-center shadow">
-          <p className="bg-logo-yellow p-4 text-xl font-bold">
-            {ARS.format(price)}
-          </p>
-          <p className="bg-secondary p-2">Precio por persona</p>
-        </section>
-        <section className="prose">
-          <h2 className="mt-0">Realizá tu reserva</h2>
-          <p>
-            ¡Reserva tu lugar ahora para no perderte de esta extraordinaria
-            aventura!
-          </p>
-          <InscriptionForm className="not-prose" salidaTitle={title} />
-        </section>
-      </main>
-    );
+
+  return (
+    <main className="container mx-auto space-y-8 py-8">
+      <section className="prose">
+        <p className="lead">{title}</p>
+        <h1>Inscripción</h1>
+        <p>
+          Formulario de inscripción para realizar la actividad{" "}
+          <Link href={`/${salida.url}`}>{title}</Link>, con fecha de inicio el{" "}
+          <strong className="text-primary">
+            {longDate.format(new Date(date))}
+          </strong>
+          .
+        </p>
+      </section>
+      <section className="mx-auto max-w-prose overflow-hidden rounded-sm text-center shadow">
+        <p className="bg-logo-yellow p-4 text-xl font-bold">
+          {ARS.format(price)}
+        </p>
+        <p className="bg-secondary p-2">Precio por persona</p>
+      </section>
+      <section className="prose">
+        <h2 className="mt-0">Realizá tu reserva</h2>
+        <p>
+          ¡Reserva tu lugar ahora para no perderte de esta extraordinaria
+          aventura!
+        </p>
+        <InscriptionForm className="not-prose" salidaTitle={title} />
+      </section>
+    </main>
+  );
 }
