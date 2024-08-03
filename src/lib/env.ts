5e10d09d5f578e31374fb3ef897b11fb87e22904
Martín Stanicio <martuytimo@gmail.com>
2024-08-03 19:34:06 -0300

    require NEXT_PUBLIC_PHONE env variable

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
-export const envVariables = z.object({});
+export const envVariables = z.object({
+  NEXT_PUBLIC_PHONE: z.string(),
+});
 
 declare global {
   namespace NodeJS {
