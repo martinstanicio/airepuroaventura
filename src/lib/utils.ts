0200e7ef1c034e7562d8476c51e4d92f0e4e95e6
Martín Stanicio <martuytimo@gmail.com>
2024-08-03 19:02:28 -0300

    add number and date formatting utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const ARS = new Intl.NumberFormat("es-AR", {
+  style: "currency",
+  currency: "ARS",
+  maximumFractionDigits: 0,
+});
+
+export const shortDate = new Intl.DateTimeFormat("es-AR", {
+  timeZone: "UTC",
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+});
+
+export const longDate = new Intl.DateTimeFormat("es-AR", {
+  dateStyle: "long",
+  timeZone: "UTC",
+});
