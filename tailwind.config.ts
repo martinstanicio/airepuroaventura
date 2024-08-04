7ee7fd51b251b10927ed160b76f9e04807a00515
Martín Stanicio <martuytimo@gmail.com>
2024-08-04 19:52:39 -0300

    remove unused colors from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,11 +56,6 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-        logo: {
-          yellow: "#f4a40c",
-          "dark-blue": "#2f4e5c",
-          "light-blue": "#34abbc",
-        },
       },
       borderRadius: {
         lg: "var(--radius)",
